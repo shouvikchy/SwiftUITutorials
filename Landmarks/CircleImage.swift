f18542dea8b61cd1f2907b16ce9121537f13b4e9
Shouvik <shouvikbs80@gmail.com>
2024-07-03 18:00:41 +0600

    basic view for park landmark

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,20 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Bayshore Communication on 3/7/24.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock").clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/).overlay{
+            Circle().stroke(.white,lineWidth: 4)
+        }.shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
